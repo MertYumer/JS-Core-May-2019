1e1fc5c52a081cd29d1f057dcccce9ae49e16dda
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-04-25 14:03:42 +0300

    Added new exercises

--- JS Fundamentals/02. Document Object Model - Exercise/06. SoftUni Quiz/solution.js
+++ JS Fundamentals/02. Document Object Model - Exercise/06. SoftUni Quiz/solution.js
@@ -1,3 +1,43 @@
 function solve() {
-	//TODO
+    let sectionElements = document.getElementById('exercise').children;
+    let sectionNumber = 0;
+    let playerPoints = 0;
+    let correctAnswers = ['2013', 'Pesho', 'Nakov'];
+    let buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons) {
+        button.addEventListener('click', clickEvent);
+    }
+
+    function clickEvent() {
+        let radioElements = sectionElements[sectionNumber].getElementsByTagName('input');
+        let selectedRadio = selectedRadioElement(radioElements);
+
+        function selectedRadioElement(radioElements) {
+            for (let radioElement of radioElements) {
+                if (radioElement.checked) {
+                    return radioElement;
+                }
+            }
+        }
+
+        if (selectedRadio.value == correctAnswers[sectionNumber]) {
+            playerPoints++;
+        }
+
+        sectionNumber++;
+
+        if (sectionNumber == sectionElements.length) {
+            let resultDivElement = document.getElementById('result');
+
+            if (playerPoints == sectionElements.length) {
+                resultDivElement.textContent = 'You are recognized as top SoftUni fan!';
+            } else {
+                resultDivElement.textContent = `You have ${playerPoints} right answers`;
+            }
+
+        } else {
+            sectionElements[sectionNumber].style.display = 'block';
+        }
+    }
 }
