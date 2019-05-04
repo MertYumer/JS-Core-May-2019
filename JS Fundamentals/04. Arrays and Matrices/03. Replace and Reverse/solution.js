660a041a0f321bf092d0fac96ae6e46e4a5fa481
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-05-04 14:06:31 +0300

    Added new exercises

--- JS Fundamentals/04. Arrays and Matrices/03. Replace and Reverse/solution.js
+++ JS Fundamentals/04. Arrays and Matrices/03. Replace and Reverse/solution.js
@@ -1,8 +1,8 @@
 function solve() {
-    let inputElement = document.getElementById('arr').value;
+    let inputValue = document.getElementById('arr').value;
     let resultElement = document.getElementById('result');
 
-    let array = JSON.parse(inputElement);
+    let array = JSON.parse(inputValue);
     let result = array
         .map(x => x.split('').reverse().join(''))
         .map(x => x.charAt(0).toUpperCase() + x.slice(1));
--- JS Fundamentals/04. Arrays and Matrices/04. Find Element/solution.js
+++ JS Fundamentals/04. Arrays and Matrices/04. Find Element/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-  // TODO
+    let searchedValue = parseInt(document.getElementById('num').value);
+    let textValue = document.getElementById('arr').value;
+    let resultElement = document.getElementById('result');
+
+    let stringArray = JSON.parse(textValue);
+    let result = [];
+
+    for (let element of stringArray) {
+        let elementExists = element.includes(searchedValue);
+        let index = element.indexOf(searchedValue);
+        result.push(elementExists + ' -> ' + index);
+    }
+
+    resultElement.textContent = result.join(',');
 }
