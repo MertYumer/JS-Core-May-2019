99ede1e261270f3a277e82c8ed772b0285666f6a
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-05-09 15:16:17 +0300

    Added new exercises

--- JS Fundamentals/05. Strings and RegEx - Exercises/03. James Bond/solution.js
+++ JS Fundamentals/05. Strings and RegEx - Exercises/03. James Bond/solution.js
@@ -1,3 +1,46 @@
 function solve() {
-  //TODO...
+    function decodeWord(word) {
+        while (word.indexOf('!') !== -1) {
+            word = word.replace('!', '1');
+        }
+
+        while (word.indexOf('%') !== -1) {
+            word = word.replace('%', '2');
+        }
+
+        while (word.indexOf('#') !== -1) {
+            word = word.replace('#', '3');
+        }
+
+        while (word.indexOf('$') !== -1) {
+            word = word.replace('$', '4');
+        }
+
+        return word.toLowerCase();
+    }
+
+    let input = JSON.parse(document.getElementById('array').value);
+    let result = document.getElementById('result');
+
+    let specialKey = input[0];
+    let pattern = new RegExp(`(?:^|\\s)(?:${specialKey})\\s+([A-Z!%$#]{8,})(?:\.|,|\\s|$)`, 'gi');
+
+    for (let i = 1; i < input.length; i++) {
+        let currentString = input[i];
+
+        while (true) {
+            let match = pattern.exec(currentString);
+
+            if (match == null) {
+                break;
+            } else if (match[1].toUpperCase() === match[1]) {
+                let decodedWord = decodeWord(match[1]);
+                currentString = currentString.replace(match[1], decodedWord);
+            }
+        }
+
+        let paragraphElement = document.createElement('p');
+        paragraphElement.textContent = currentString;
+        result.appendChild(paragraphElement);
+    }
 }
--- JS Fundamentals/05. Strings and RegEx - Exercises/04. Airport Check/solution.js
+++ JS Fundamentals/05. Strings and RegEx - Exercises/04. Airport Check/solution.js
@@ -1,3 +1,36 @@
 function solve() {
- //TODO...
+    let input = document.getElementById('string').value.split(', ');
+    let text = input[0];
+    let command = input[1];
+    let result = document.getElementById('result');
+
+    let namePattern = new RegExp(` (([A-Z]([A-Za-z]*)?)(-[A-Z][A-Za-z]*\\.)?-([A-Z][A-Za-z]*)?) `, 'g');
+    let airportPattern = new RegExp(' ([A-Z]{3}\\/[A-Z]{3}) ', 'g');
+    let flightNumberPattern = new RegExp(' ([A-Z]{1,3}\\d{1,5}) ', 'g');
+    let companyPattern = new RegExp('- ([A-Z][A-Za-z]*\\*[A-Z][A-Za-z]*) ', 'g');
+
+    let name = namePattern.exec(text)[1].toString().replace(/\-/g, ' ');
+    let flightNumber = flightNumberPattern.exec(text)[1].toString();
+    let airports = airportPattern.exec(text)[1].toString().split('/');
+    let fromAirport = airports[0];
+    let toAirport = airports[1];
+    let company = companyPattern.exec(text)[1].toString().replace('*', ' ');
+
+    switch (command) {
+        case 'name':
+            result.textContent = `Mr/Ms, ${name}, have a nice flight!`;
+            break;
+
+        case 'flight':
+            result.textContent = `Your flight number ${flightNumber} is from ${fromAirport} to ${toAirport}.`;
+            break;
+
+        case 'company':
+            result.textContent = `Have a nice flight with ${company}.`;
+            break;
+
+        case 'all':
+            result.textContent = `Mr/Ms, ${name}, your flight number ${flightNumber} is from ${fromAirport} to ${toAirport}. Have a nice flight with ${company}.`;
+            break;
+    }
 }
