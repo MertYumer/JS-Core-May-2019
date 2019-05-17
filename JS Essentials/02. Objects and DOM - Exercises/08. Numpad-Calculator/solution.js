4df6bd9b06f1d74ab59124224163d394121110c1
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-05-17 17:36:44 +0300

    Added unfinished exercises

--- JS Essentials/02. Objects and DOM - Exercises/08. Numpad-Calculator/solution.js
+++ JS Essentials/02. Objects and DOM - Exercises/08. Numpad-Calculator/solution.js
@@ -1,4 +1,46 @@
 function solve() {
+    function clearFields() {
+        expressionOutput.textContent = '';
+        resultOutput.textContent = '';
+    }
 
-    //TODO...
+    function showValue() {
+        let lastCharacter = expressionOutput.textContent[expressionOutput.textContent.length - 1];
+
+        if ((this.value === '/' || this.value === '-' || this.value === '*' || this.value === '+') ||
+            (lastCharacter === '/' || lastCharacter === '-' || lastCharacter === '*' || lastCharacter === '+')) {
+            expressionOutput.textContent += ` ${this.value}`;
+        } else {
+            expressionOutput.textContent += this.value;
+        }
+    }
+
+    function showResult() {
+        try {
+            let result = eval(expressionOutput.textContent);
+
+            if (isNaN(result)) {
+                resultOutput.textContent = 'NaN';
+            } else {
+                resultOutput.textContent = result;
+            }
+
+        } catch (e) {
+            resultOutput.textContent = 'NaN';
+        }
+    }
+
+    let expressionOutput = document.getElementById('expressionOutput');
+    let resultOutput = document.getElementById('resultOutput');
+
+    let buttons = document.getElementsByTagName('button');
+    buttons[0].addEventListener('click', clearFields);
+
+    for (let i = 1; i < buttons.length; i++) {
+        if (i === 15) {
+            buttons[i].addEventListener('click', showResult)
+        } else {
+            buttons[i].addEventListener('click', showValue);
+        }
+    }
 }
--- JS Essentials/02. Objects and DOM - Exercises/09. Order-the-Names/solution.js
+++ JS Essentials/02. Objects and DOM - Exercises/09. Order-the-Names/solution.js
@@ -1,4 +1,27 @@
 function solve() {
+    function addToList() {
+        let inputElement = document.getElementsByTagName('input')[0];
+        let input = inputElement.value;
+        let charCode = input.charCodeAt(0);
 
-    //TODO...
+        if ((charCode >= 65 && charCode <= 90)
+        || (charCode >= 97 && charCode <= 122)) {
+            let columnNumber = charCode <= 90
+                ? charCode - 65
+                : charCode - 97;
+
+            if (columns[columnNumber].textContent) {
+                columns[columnNumber].textContent += `, ${input}`;
+            } else {
+                columns[columnNumber].textContent += input;
+            }
+        }
+
+        inputElement.value = '';
+    }
+
+    let buttonElement = document.getElementsByTagName('button')[0];
+    let columns = document.getElementsByTagName('li');
+
+    buttonElement.addEventListener('click', addToList);
 }
