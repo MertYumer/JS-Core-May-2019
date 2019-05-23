ef1b00c25c075c235e329dae79361a86c5b8b4f2
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-05-23 13:55:09 +0300

    Added new exercises

---
+++ JS Advanced - June 2019/01. Advanced Functions - Exercises/04-personal-bmi.js
@@ -0,0 +1,28 @@
+function solve(name, age, weight, height) {
+    age = +age;
+    weight = +weight;
+    height = +height;
+
+    let person = {};
+    person.name = name;
+    person.personalInfo = {
+        age: age,
+        weight: weight,
+        height: height
+    };
+
+    person.BMI = Math.round(weight / (height * height / 10000));
+
+    if (person.BMI < 18.5) {
+        person.status = "underweight";
+    } else if (person.BMI < 25) {
+        person.status = "normal";
+    } else if (person.BMI < 30) {
+        person.status = "overweight";
+    } else if (person.BMI >= 30) {
+        person.status = "obese";
+        person.recommendation = "admission required";
+    }
+
+    return person;
+}
---
+++ JS Advanced - June 2019/01. Advanced Functions - Exercises/05-vector-math.js
@@ -0,0 +1,9 @@
+(() => {
+    let add = (firstVector, secondVector) => [firstVector[0] + secondVector[0], firstVector[1] + secondVector[1]];
+    let multiply = (vector, scalar) => [vector[0] * scalar, vector[1] * scalar];
+    let length = (vector) => Math.sqrt((vector[0] * vector[0]) + (vector[1] * vector[1]));
+    let dot = (firstVector, secondVector) => (firstVector[0] * secondVector[0]) + (firstVector[1] * secondVector[1]);
+    let cross = (firstVector, secondVector) => (firstVector[0] * secondVector[1]) - (firstVector[1] * secondVector[0]);
+
+    return {add, multiply, length, dot, cross};
+})();
