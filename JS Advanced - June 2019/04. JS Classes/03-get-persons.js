6f24909c593cd734eaa2635f90f62f045941ff6e
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-06-15 13:39:21 +0300

    Added new exercises

---
+++ JS Advanced - June 2019/04. JS Classes/03-get-persons.js
@@ -0,0 +1,22 @@
+function getPersons() {
+    class Person {
+        constructor(firstName, lastName, age, email) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email;
+        }
+
+        toString() {
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+        }
+    }
+
+    const firstPerson = new Person('Anna', 'Simpson', 22, 'anna@yahoo.com');
+    const secondPerson = new Person('SoftUni');
+    const thirdPerson = new Person('Stephan', 'Johnson', 25);
+    const fourthPerson = new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com');
+    const people = [firstPerson, secondPerson, thirdPerson, fourthPerson];
+
+    return people;
+}
