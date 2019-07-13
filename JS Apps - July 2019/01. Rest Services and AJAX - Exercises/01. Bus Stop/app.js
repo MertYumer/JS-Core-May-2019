5ffea1154ab54ebeb3f599f275ba4b25695a3ec9
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-07-13 14:48:33 +0300

    Added new exercises

--- JS Apps - July 2019/01. Rest Services and AJAX - Exercises/01. Bus Stop/app.js
+++ JS Apps - July 2019/01. Rest Services and AJAX - Exercises/01. Bus Stop/app.js
@@ -1,54 +1,37 @@
 function getInfo() {
-    let stopId = document.getElementById('stopId').value;
-    clearBusList();
-    request('GET', getURL(stopId), true);
-}
-
-function clearBusList() {
-    const busList = document.getElementById('buses');
-    busList.innerHTML = '';
-}
-
-function request(method, url, async) {
-    let request = new XMLHttpRequest();
-
-    request.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            onGetBusInfoSuccess(JSON.parse(this.response));
-
-        } else if (this.readyState == 4 && this.status !== 200) {
-            onGetBusInfoError();
-        }
-    };
-
-    request.open(method, url, async);
-    request.send();
-}
-
-function getURL(stopId) {
-    const url = `https://judgetests.firebaseio.com/businfo/${stopId}.json`;
-    return url;
-}
-
-function onGetBusInfoSuccess(data) {
+    const stopId = document.getElementById('stopId').value;
     const stopName = document.getElementById('stopName');
-    stopName.textContent = data.name;
-    const busList = document.getElementById('buses');
+    const busesList = document.getElementById('buses');
+    busesList.innerHTML = '';
+    const url = `https://judgetests.firebaseio.com/businfo/${stopId}.json`;
 
-    for (const bus in data.buses) {
-        if (data.buses.hasOwnProperty(bus)) {
-            let busId = bus;
-            let time = data.buses[bus];
+    request('GET', url, true);
 
-            let currentBus = document.createElement('li');
-            currentBus.textContent = `Bus ${busId} arrives in ${time} minutes`;
+    function request(method, url, async) {
+        const request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200){
+                const data = JSON.parse(this.response);
+                const {name, buses} = data;
+                stopName.textContent = name;
 
-            busList.appendChild(currentBus);
-        }
+                for (const bus in buses) {
+                    const busId = bus;
+                    const time = buses[bus];
+
+                    const currentBus = document.createElement('li');
+                    currentBus.textContent = `Bus ${busId} arrives in ${time} minutes`;
+                    busesList.appendChild(currentBus);
+                }
+
+            } else if (this.readyState == 4 && this.status !== 200){
+                stopName.textContent = 'Error';
+            }
+        };
+
+        request.open(method, url, async);
+        request.send();
     }
 }
 
-function onGetBusInfoError() {
-    const stopName = document.getElementById('stopName');
-    stopName.textContent = 'Error';
-}
+
--- JS Apps - July 2019/01. Rest Services and AJAX - Exercises/03. Phonebook/app.js
+++ JS Apps - July 2019/01. Rest Services and AJAX - Exercises/03. Phonebook/app.js
@@ -1,5 +1,97 @@
 function attachEvents() {
-    console.log('TODO...');
+    clearPhoneBook();
+
+    const loadButton = document.getElementById('btnLoad');
+    loadButton.addEventListener('click', loadContacts);
+
+    const createButton = document.getElementById('btnCreate');
+    createButton.addEventListener('click', createContact);
+
+    const url = `https://phonebook-nakov.firebaseio.com/phonebook.json`;
+
+    function loadContacts() {
+        clearPhoneBook();
+
+        fetch(url)
+            .then(request => request.json())
+            .then(data => {
+                const contacts = Object.values(data);
+
+                for (const contact of contacts) {
+                    const name = contact.person;
+                    const phoneNumber = contact.phone;
+
+                    const deleteButton = document.createElement('button');
+                    deleteButton.textContent = 'Delete';
+
+                    const li = document.createElement('li');
+                    li.textContent = `${name}: ${phoneNumber} `;
+                    li.appendChild(deleteButton);
+
+                    document.getElementById('phonebook').appendChild(li);
+
+                    deleteButton.addEventListener('click', (e) => {
+                        const currentContact = e.target.parentNode;
+                        const contactInfo = currentContact.textContent
+                            .split(' ').filter(x => x !== '');
+
+                        const name = contactInfo[0];
+                        const number = contactInfo[1];
+                        let searchedId = '';
+
+                        const contactsIds = Object.keys(data);
+
+                        for (const id of contactsIds) {
+                            if (data[id].person === name
+                                && data[id].phone === number) {
+                                searchedId = id;
+                                break;
+                            }
+                        }
+
+                        currentContact.remove();
+
+                        const idUrl = 'https://phonebook-nakov.firebaseio.com/phonebook'
+                        + '/' + searchedId + '.json';
+
+                        fetch(idUrl, {
+                            method: 'delete',
+                            body: JSON.stringify(searchedId)
+                        })
+                            .then(resolve => resolve.json());
+                    });
+                }
+            });
+    }
+
+    function createContact() {
+        clearPhoneBook();
+
+        const name = document.getElementById('person');
+        const number = document.getElementById('phone');
+
+        if (name.value && number.value) {
+            let currentPerson = {
+                person: name.value,
+                phone: number.value
+            };
+
+            fetch(url, {
+                method: 'post',
+                body: JSON.stringify(currentPerson)
+            })
+                .then(res => res.json());
+        }
+
+        loadContacts();
+
+        name.value = '';
+        number.value = '';
+    }
+}
+
+function clearPhoneBook() {
+    document.getElementById('phonebook').innerHTML = '';
 }
 
 attachEvents();
