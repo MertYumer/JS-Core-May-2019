5142f420ca9ba54b6192adf9e3f297c41ba1ae3d
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-07-18 17:48:32 +0300

    Added part from 02. Fisher-Game

--- JS Apps - July 2019/02. Asynchronous Programming - Exercises/02. Fisher-Game/app.js
+++ JS Apps - July 2019/02. Asynchronous Programming - Exercises/02. Fisher-Game/app.js
@@ -1,5 +1,42 @@
 function attachEvents() {
-    console.log('TODO...');
+    const elements = {
+        loadButton: document.querySelector('button.load'),
+        addButton: document.querySelector('button.add'),
+        /*updateButton: document.querySelector('button.update'),
+        deleteButton: document.querySelector('button.delete')*/
+    };
+
+    elements.loadButton.addEventListener('click', loadAllCatches);
+    elements.addButton.addEventListener('click', createCatch);
+
+    /*elements.updateButton.addEventListener('click', updateCatch);
+    elements.deleteButton.addEventListener('click', deleteCatch);*/
+
+    function loadAllCatches() {
+        fetch('https://fisher-game.firebaseio.com/catches.json', { method: 'GET' })
+            .then(handler)
+            .then(console.log);
+    }
+
+    function createCatch() {
+
+    }
+
+    /*function updateCatch() {
+
+    }
+
+    function deleteCatch() {
+
+    }*/
+
+    function handler(response) {
+        if (response.status >= 400) {
+            throw new Error(`Something went wrong. Error ${response.statusText}`)
+        }
+
+        return response.json();
+    }
 }
 
 attachEvents();
