bc83502405d2db665198191bd629a547760adc1f
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-07-19 14:58:01 +0300

    Added new exercise

--- JS Apps - July 2019/02. Asynchronous Programming - Exercises/02. Fisher-Game/app.js
+++ JS Apps - July 2019/02. Asynchronous Programming - Exercises/02. Fisher-Game/app.js
@@ -2,33 +2,12 @@ function attachEvents() {
     const elements = {
         loadButton: document.querySelector('button.load'),
         addButton: document.querySelector('button.add'),
-        /*updateButton: document.querySelector('button.update'),
-        deleteButton: document.querySelector('button.delete')*/
+        catches: document.getElementById('catches')
     };
 
+    elements.catches.children[0].style.display = 'none';
     elements.loadButton.addEventListener('click', loadAllCatches);
     elements.addButton.addEventListener('click', createCatch);
-
-    /*elements.updateButton.addEventListener('click', updateCatch);
-    elements.deleteButton.addEventListener('click', deleteCatch);*/
-
-    function loadAllCatches() {
-        fetch('https://fisher-game.firebaseio.com/catches.json', { method: 'GET' })
-            .then(handler)
-            .then(console.log);
-    }
-
-    function createCatch() {
-
-    }
-
-    /*function updateCatch() {
-
-    }
-
-    function deleteCatch() {
-
-    }*/
 
     function handler(response) {
         if (response.status >= 400) {
@@ -37,7 +16,93 @@ function attachEvents() {
 
         return response.json();
     }
+
+    function loadAllCatches() {
+        fetch('https://fisher-game.firebaseio.com/catches.json', {method: 'GET'})
+            .then(handler)
+            .then(showAllCatches);
+    }
+
+    function showAllCatches(data) {
+        Object.keys(data).forEach(key => {
+            const catchElement = elements.catches.children[0].cloneNode(true);
+            catchElement.style.display = 'inline-block';
+
+            catchElement.setAttribute('data-id', key);
+            catchElement.querySelector('input.angler').value = data[key].angler;
+            catchElement.querySelector('input.weight').value = data[key].weight;
+            catchElement.querySelector('input.species').value = data[key].species;
+            catchElement.querySelector('input.location').value = data[key].location;
+            catchElement.querySelector('input.bait').value = data[key].bait;
+            catchElement.querySelector('input.captureTime').value = data[key].captureTime;
+            catchElement.querySelector('button.update').addEventListener('click', updateCatch);
+            catchElement.querySelector('button.delete').addEventListener('click', deleteCatch);
+
+            elements.catches.appendChild(catchElement);
+        });
+
+        function updateCatch(event) {
+            const catchId = event.currentTarget.parentNode.getAttribute('data-id');
+            const currentCatch = event.currentTarget.parentNode;
+
+            const data = Array.from(currentCatch.children)
+                .filter(e => e.tagName === 'INPUT')
+                .reduce((acc, curr) => {
+                    const prop = curr.className;
+                    acc[prop] = curr.value;
+                    return acc;
+                }, {});
+
+            const headers = {
+                method: 'PUT',
+                body: JSON.stringify(data)
+            };
+
+            fetch(`https://fisher-game.firebaseio.com/catches/${catchId}.json`, headers)
+                .then(handler)
+                .then(data => {
+                    elements.loadButton.click();
+                });
+        }
+
+        function deleteCatch(event) {
+            const catchId = event.currentTarget.parentNode.getAttribute('data-id');
+            const currentCatch = event.currentTarget.parentNode;
+
+            const headers = {
+                method: 'DELETE'
+            };
+
+            fetch(`https://fisher-game.firebaseio.com/catches/${catchId}.json`, headers)
+                .then(handler)
+                .then(data => {
+                    currentCatch.remove();
+                });
+        }
+    }
+
+    function createCatch() {
+        const currentCatch = document.querySelector('fieldset#addForm');
+
+        const data = Array.from(currentCatch.children)
+            .filter(e => e.tagName === 'INPUT')
+            .reduce((acc, curr) => {
+                const prop = curr.className;
+                acc[prop] = curr.value;
+                return acc;
+            }, {});
+
+        const headers = {
+            method: 'POST',
+            body: JSON.stringify(data)
+        };
+
+        fetch('https://fisher-game.firebaseio.com/catches.json', headers)
+            .then(handler)
+            .then(data => {
+                elements.loadButton.click();
+            });
+    }
 }
 
 attachEvents();
-
