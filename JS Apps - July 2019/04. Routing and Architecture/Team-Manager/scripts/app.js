7189a26612126938437c6f8a0dc05617af7862fa
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-07-29 19:12:42 +0300

    Added unfinished Exam Preparation - 29.07.2019

--- JS Apps - July 2019/04. Routing and Architecture/Team-Manager/scripts/app.js
+++ JS Apps - July 2019/04. Routing and Architecture/Team-Manager/scripts/app.js
@@ -7,10 +7,10 @@ const app = Sammy('#main', function () {
     //User
     this.get('#/register', userController.getRegister);
     this.get('#/login', userController.getLogin);
+    this.get('#/logout', userController.logout);
 
     this.post('#/register', userController.postRegister);
     this.post('#/login', userController.postLogin);
-    this.get('#/logout', userController.logout);
 });
 
 (() => {
